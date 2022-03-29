59a73e1f5538fa2f753a53aa59d9b74e35161126
bilaltahiraziz <bilal.aziz@live.com>
2022-03-29 21:37:23 +0100

    Added Api changes Production and Deployment

--- app/api.js
+++ app/api.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const store = require('./store.js')
+const config = require('./config.js')
 
 const signUp = function (data) {
   return $.ajax({
     method: 'POST',
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-up',
+    url: config.apiUrl + '/sign-up',
     data
   })
 }
@@ -13,7 +14,7 @@ const signUp = function (data) {
 const signIn = function (data) {
   return $.ajax({
     method: 'POST',
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
+    url: config.apiUrl + '/sign-in',
     data
   })
 }
@@ -21,7 +22,7 @@ const signIn = function (data) {
 const signOut = function () {
   return $.ajax({
     method: 'DELETE',
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-out',
+    url: config.apiUrl + '/sign-out',
     headers: {
       Authorization: 'Bearer ' + store.user.token
     }
@@ -32,7 +33,7 @@ const signOut = function () {
 const gameStatus = function () {
   return $.ajax({
     method: 'GET',
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/games',
+    url: config.apiUrl + '/games',
     headers: {
       Authorization: 'Bearer ' + store.user.token
     }
@@ -42,7 +43,7 @@ const gameStatus = function () {
 const createGame = function () {
   return $.ajax({
     method: 'POST',
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/games',
+    url: config.apiUrl + '/games',
     headers: {
       Authorization: 'Bearer ' + store.user.token
     },
@@ -53,7 +54,7 @@ const createGame = function () {
 const playerMove = function (index, value, gameOver) {
   return $.ajax({
     method: 'PATCH',
-    url: 'https://tic-tac-toe-api-development.herokuapp.com/games/' + store.game._id,
+    url: config.apiUrl + '/games/' + store.game._id,
     headers: {
       Authorization: 'Bearer ' + store.user.token
     },
